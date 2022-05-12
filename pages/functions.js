36e94e56301faa621b0dedadf18b4bb73996bd7e
BrunoWanderson <brunowanderson8@gmail.com>
2022-05-12 19:48:27 -0300

    commit: v0.7.1 (index, contact, about, freelancer, projects, project, submit-offer, sign-in, sign-up) complete. (login with Js).

--- pages/functions.js
+++ pages/functions.js
@@ -1,6 +1,7 @@
 
 
 function cadastrar (){
+    localStorage.name = document.getElementById("sign-name").value;
     localStorage.email = document.getElementById("sign-email").value;
     localStorage.pass = document.getElementById("sign-pass").value;
 }
@@ -10,7 +11,6 @@ function logar (){
     var logP = document.getElementById("pass-login").value;
     var logPass = logP.toString();
     var logMail = logM.toString();
-
 
     var url = ((logMail == localStorage.email) && 
     (logPass == localStorage.pass)) ? (alert("Dados corretos"), "index-log.html") : 
